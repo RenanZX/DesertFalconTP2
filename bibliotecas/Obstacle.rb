26da6e8077674cc0b50cfc63411ea279cf2392d0
matheusrg21 <matheusrg007@gmail.com>
2017-11-17 22:47:19 -0200

    Adicionando comentários para documentação ao arquivo que faltou

--- bibliotecas/Obstacle.rb
+++ bibliotecas/Obstacle.rb
@@ -3,12 +3,19 @@ require_relative 'GameObject'
 require_relative 'Sprite'
 require_relative 'Box'
 
+# Classe que representa os obstáculos
 class Obstacle < GameObject
+
+  # Método construtor da classe Obstáculo
+  # @param x [Numeric] coordenada x onde o obstáculo será desenhado na tela
+  # @param y [Numeric] coordenada y onde o obstáculo será desenhado na tela
+  # @param z [Numeric] coordenada z onde o obstáculo será desenhado na tela
   def initialize(x, y, z)
     @sprite = Sprite.new 'obstacle.png'
     super
   end
 
+  # Método de manipulação do movimento do obstáculo
   def move
     @velx = Gosu.offset_x(225, 1)
     @vely = Gosu.offset_y(225, 1)
@@ -16,6 +23,7 @@ class Obstacle < GameObject
     @box.y += @vely
   end
 
+  # Método que atualiza a movimentação do obstáculo
   def update
     move
   end
